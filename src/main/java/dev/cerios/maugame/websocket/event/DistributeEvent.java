7f5dd6858b78b2c7423ac7486469448c4050d53f
pepab0t <cernikjos@gmail.com>
2025-05-05 22:04:04 +0200

    chore: progress

--- src/main/java/dev/cerios/maugame/websocket/event/DistributeEvent.java
+++ src/main/java/dev/cerios/maugame/websocket/event/DistributeEvent.java
@@ -5,15 +5,16 @@ import lombok.Getter;
 import lombok.ToString;
 import org.springframework.context.ApplicationEvent;
 
+import java.util.Collection;
 import java.util.List;
 
 @Getter
 @ToString
 public class DistributeEvent extends CustomApplicationEvent {
-    private final List<String> players;
-    private final List<Action> actions;
+    private final Collection<String> players;
+    private final Collection<Action> actions;
 
-    public DistributeEvent(Object source, List<String> players, List<Action> actions) {
+    public DistributeEvent(Object source, Collection<String> players, Collection<Action> actions) {
         super(source);
         this.players = players;
         this.actions = actions;
