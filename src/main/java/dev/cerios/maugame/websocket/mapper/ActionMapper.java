e88a32776051ea0c7cbdeafe76dc8954028517c1
cernikjos <cernikjos@gmail.com>
2025-07-13 10:13:25 +0200

    feat: map actions to dtos

--- src/main/java/dev/cerios/maugame/websocket/mapper/ActionMapper.java
+++ src/main/java/dev/cerios/maugame/websocket/mapper/ActionMapper.java
@@ -1,10 +1,12 @@
 package dev.cerios.maugame.websocket.mapper;
 
+import dev.cerios.maugame.mauengine.game.Player;
 import dev.cerios.maugame.mauengine.game.action.*;
 import dev.cerios.maugame.websocket.dto.action.*;
 import dev.cerios.maugame.websocket.dto.player.PlayerDto;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
@@ -15,50 +17,57 @@ public abstract class ActionMapper {
     protected PlayerMapper playerMapper;
 
     @Mapping(target = "playerDto", expression = "java(playerMapper.toPublicDto(action.player()))")
-    abstract PlayerActionDto toDto(ActivateAction action);
+    public abstract PlayerActionDto toDto(ActivateAction action);
 
     @Mapping(target = "playerDto", expression = "java(playerMapper.toPublicDto(action.player()))")
-    abstract PlayerActionDto toDto(DeactivateAction action);
+    public abstract PlayerActionDto toDto(DeactivateAction action);
 
-    abstract DrawActionDto toDto(DrawAction action);
+    @Mapping(target = "cards", source = "action.cardsDrawn")
+    public abstract DrawActionDto toDto(DrawAction action);
 
-    abstract ActionDto toDto(EndAction action);
-
-    @Mapping(target = "playerDto", expression = "java(playerMapper.toPublicDto(action.player()))")
-    abstract HiddenDrawDto toDto(HiddenDrawAction action);
+    public abstract ActionDto toDto(EndAction action);
 
     @Mapping(target = "playerDto", expression = "java(playerMapper.toPublicDto(action.player()))")
-    abstract PlayerActionDto toDto(LoseAction action);
+    public abstract HiddenDrawDto toDto(HiddenDrawAction action);
 
     @Mapping(target = "playerDto", expression = "java(playerMapper.toPublicDto(action.player()))")
-    abstract PlayerActionDto toDto(PassAction action);
+    public abstract PlayerActionDto toDto(LoseAction action);
 
     @Mapping(target = "playerDto", expression = "java(playerMapper.toPublicDto(action.player()))")
-    abstract PlayCardActionDto toDto(PlayCardAction action);
+    public abstract PlayerActionDto toDto(PassAction action);
+
+    @Mapping(target = "playerDto", expression = "java(playerMapper.toPublicDto(action.player()))")
+    @Mapping(target = "type", source = "action", qualifiedByName = "typeGetter")
+    public abstract PlayCardActionDto toDto(PlayCardAction action);
 
     @Mapping(target = "players", expression = "java(action.players().stream().map(p -> p.getUsername()).toList())")
-    abstract ShowPlayersActionDto toDto(PlayersAction action);
+    public abstract ShowPlayersActionDto toDto(PlayersAction action);
 
     @Mapping(target = "playerDto", expression = "java(playerMapper.toPublicDto(action.player()))")
-    abstract PlayerActionDto toDto(PlayerShiftAction action);
+    public abstract PlayerActionDto toDto(PlayerShiftAction action);
 
     @Mapping(target = "playerDto", expression = "java(mapRegisterPlayer(action))")
-    abstract PlayerActionDto toDto(RegisterAction action);
+    public abstract PlayerActionDto toDto(RegisterAction action);
 
     @Mapping(target = "playerDto", expression = "java(playerMapper.toPublicDto(action.player()))")
-    abstract PlayerActionDto toDto(RemovePlayerAction action);
+    public abstract PlayerActionDto toDto(RemovePlayerAction action);
 
     @Mapping(target = "players", expression = "java(action.playerRank().stream().toList())")
-    abstract ShowPlayersActionDto toDto(SendRankAction action);
+    public abstract ShowPlayersActionDto toDto(SendRankAction action);
 
-    abstract GameIdActionDto toDto(StartAction action);
+    public abstract GameIdActionDto toDto(StartAction action);
 
-    abstract CardActionDto toDto(StartPileAction action);
+    public abstract CardActionDto toDto(StartPileAction action);
 
     @Mapping(target = "playerDto", expression = "java(playerMapper.toPublicDto(action.player()))")
-    abstract PlayerActionDto toDto(WinAction action);
+    public abstract PlayerActionDto toDto(WinAction action);
 
     protected PlayerDto mapRegisterPlayer(RegisterAction action) {
         return action.isMe() ? playerMapper.toPrivateDto(action.player()) : playerMapper.toPublicDto(action.player());
+    }
+
+    @Named("typeGetter")
+    protected Action.ActionType getType(Action action) {
+        return action.getType();
     }
 }
