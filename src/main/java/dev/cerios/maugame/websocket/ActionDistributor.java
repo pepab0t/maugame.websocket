bd5c103153ebfc72520636e32a6f5c896461d10f
cernikjos <cernikjos@gmail.com>
2025-07-13 10:22:43 +0200

    feat: use ActionMapper to distribute action dtos

--- src/main/java/dev/cerios/maugame/websocket/ActionDistributor.java
+++ src/main/java/dev/cerios/maugame/websocket/ActionDistributor.java
@@ -2,7 +2,10 @@ package dev.cerios.maugame.websocket;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.cerios.maugame.mauengine.game.Player;
-import dev.cerios.maugame.mauengine.game.action.Action;
+import dev.cerios.maugame.mauengine.game.action.*;
+import dev.cerios.maugame.websocket.dto.action.ActionDto;
+import dev.cerios.maugame.websocket.mapper.ActionMapper;
+import dev.cerios.maugame.websocket.mapper.ActionMapperImpl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -23,19 +26,43 @@ public class ActionDistributor {
     private final ExecutorService executor;
     private final SessionGameBridge bridge;
     private final ObjectMapper objectMapper;
+    private final ActionMapper actionMapper;
     private final Lock lock = new ReentrantLock();
 
     public void distribute(Player player, Action action) {
         executor.execute(() -> {
             var session = bridge.getSession(player);
+            var dto = mapAction(action);
             try {
                 lock.lock();
-                session.sendMessage(new TextMessage(objectMapper.writeValueAsString(action)));
+                session.sendMessage(new TextMessage(objectMapper.writeValueAsString(dto)));
             } catch (IOException ex) {
                 log.warn("cannot send proper message", ex);
             } finally {
                 lock.unlock();
             }
         });
+    }
+    
+    private ActionDto mapAction(Action action) {
+        return switch (action) {
+            case ActivateAction a -> actionMapper.toDto(a);
+            case DeactivateAction a -> actionMapper.toDto(a);
+            case DrawAction a -> actionMapper.toDto(a);
+            case EndAction a -> actionMapper.toDto(a);
+            case HiddenDrawAction a -> actionMapper.toDto(a);
+            case LoseAction a -> actionMapper.toDto(a);
+            case PassAction a -> actionMapper.toDto(a);
+            case PlayCardAction a -> actionMapper.toDto(a);
+            case PlayersAction a -> actionMapper.toDto(a);
+            case PlayerShiftAction a -> actionMapper.toDto(a);
+            case RegisterAction a -> actionMapper.toDto(a);
+            case RemovePlayerAction a -> actionMapper.toDto(a);
+            case SendRankAction a -> actionMapper.toDto(a);
+            case StartAction a -> actionMapper.toDto(a);
+            case StartPileAction a -> actionMapper.toDto(a);
+            case WinAction a -> actionMapper.toDto(a);
+            default -> throw new IllegalStateException("Unexpected value: " + action);
+        };
     }
 }
