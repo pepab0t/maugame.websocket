fa2c1d3b2d6311f19a7d399184b8adf088bd5a2e
cernikjos <cernikjos@gmail.com>
2025-08-04 12:55:12 +0200

    build: configurable property from ENV

--- src/main/java/dev/cerios/maugame/websocket/MauWebsocketApplication.java
+++ src/main/java/dev/cerios/maugame/websocket/MauWebsocketApplication.java
@@ -1,11 +1,21 @@
 package dev.cerios.maugame.websocket;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@Slf4j
 public class MauWebsocketApplication {
     public static void main(String[] args) {
-        var app = SpringApplication.run(MauWebsocketApplication.class, args);
+        SpringApplication.run(MauWebsocketApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(MauSettings settings) {
+        return (args) -> log.info("started with settings: {}", settings);
     }
 }
