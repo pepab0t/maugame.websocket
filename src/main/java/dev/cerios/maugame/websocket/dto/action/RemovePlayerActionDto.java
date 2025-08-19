4098457c04ae549c03f7764b2ad4cdb623f1a431
cernikjos <cernikjos@gmail.com>
2025-08-19 16:03:30 +0200

    feat: dto serialization rule

--- src/main/java/dev/cerios/maugame/websocket/dto/action/RemovePlayerActionDto.java
+++ src/main/java/dev/cerios/maugame/websocket/dto/action/RemovePlayerActionDto.java
@@ -1,5 +1,6 @@
 package dev.cerios.maugame.websocket.dto.action;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import dev.cerios.maugame.mauengine.game.action.Action;
 import dev.cerios.maugame.websocket.dto.player.PlayerDto;
 import lombok.EqualsAndHashCode;
@@ -11,6 +12,7 @@ import lombok.ToString;
 @Getter
 public class RemovePlayerActionDto extends PlayerActionDto {
 
+    @JsonInclude(JsonInclude.Include.NON_DEFAULT)
     private final int recycledCards;
 
     public RemovePlayerActionDto(Action.ActionType type, PlayerDto playerDto, int recycledCards) {
